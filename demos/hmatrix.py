6be829eea257deac6148259b59fb1cae4444080d
Weggler <79197980+Weggler@users.noreply.github.com>
2023-11-30 09:43:42 +0100

    test script for hmatrix assembling

---
+++ demos/hmatrix.py
@@ -0,0 +1,24 @@
+#  testfile for checking hmatrix generation
+
+
+import sys
+sys.path.append('../build')
+
+from netgen.occ import *
+from ngsolve import *
+from ngsolve.webgui import Draw
+from libbem import *
+
+
+sp = Sphere( (0,0,0), 1)
+mesh = Mesh( OCCGeometry(sp).GenerateMesh(maxh=0.5)).Curve(1)
+# mesh = Mesh(unit_cube.GenerateMesh(maxh=0.3))
+
+fes = SurfaceL2(mesh, order=3, dual_mapping=True)
+u,v = fes.TnT()
+
+a8 = BilinearForm(fes)
+SingleLayerPotentialOperator(a8, intorder=8)
+a8.Assemble();
+# print (a8.mat)
+
