49bf6ff2b6ce99b6ee58b2f9cc0082c7c29ac732
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-12-19 06:43:10 +0100

    move intrules to extra file

--- src/ngbem.cpp
+++ src/ngbem.cpp
@@ -1,6 +1,7 @@
 #include <solve.hpp>        // everything from ngsolve
 #include <cmath>
 
+#include "intrules.hpp"
 #include "ngbem.hpp"
 #include "hmat.hpp"
 
@@ -8,125 +9,6 @@
 namespace ngbem
 {
 
-  // x, y in triangle [(0,0), (1,0), (0,1)]
-  tuple<Array<Vec<2>>, Array<Vec<2>>, Array<double>> IdenticPanelIntegrationRule (int order)
-  {
-    IntegrationRule irsegm(ET_SEGM, order);
-    IntegrationRule irhex (ET_HEX, order);    
-
-    Array<Vec<4>> Duffies;
-    Array<double> weights;
-
-    // Sauter-Schwab integration points in triangle [(0,0), (1,0), (1,1)]
-    // page 240 German edition    
-    for (auto ipeta : irhex)
-      for (auto ipxi : irsegm)
-        {
-          double e1 = ipeta(0);
-          double e2 = ipeta(1);
-          double e3 = ipeta(2);
-          double xi = ipxi(0);
-
-          Duffies.Append (xi*Vec<4>(1, 1-e1+e1*e2, 1-e1*e2*e3, 1-e1));
-          Duffies.Append (xi*Vec<4>(1-e1*e2*e3, 1-e1, 1, 1-e1+e1*e2));
-          Duffies.Append (xi*Vec<4>(1, e1*(1-e2+e2*e3), 1-e1*e2, e1*(1-e2) ));
-          Duffies.Append (xi*Vec<4>(1-e1*e2, e1*(1-e2), 1, e1*(1-e2+e2*e3) ));
-          Duffies.Append (xi*Vec<4>(1-e1*e2*e3, e1*(1-e2*e3), 1, e1*(1-e2) ));
-          Duffies.Append (xi*Vec<4>(1, e1*(1-e2), 1-e1*e2*e3, e1*(1-e2*e3) ));
-          for (int j = 0; j < 6; j++)
-            weights.Append (xi*xi*xi*e1*e1*e2 * ipeta.Weight()*ipxi.Weight());
-        }
-
-	
-    // trafo to [(0,0), (1,0), (0,1)]
-    Array<Vec<2>> ipx, ipy;
-    for (auto ip : Duffies)
-      {
-        ipx += Vec<2>(ip(0)-ip(1), ip(1));
-        ipy += Vec<2>(ip(2)-ip(3), ip(3));
-      }
-
-    return tuple { ipx, ipy, weights };
-  }
-
-
-  // x, y in triangle [(0,0), (1,0), (0,1)]
-  // x=(0,0) and y=(0,0) are common vertices
-  tuple<Array<Vec<2>>, Array<Vec<2>>, Array<double>> CommonVertexIntegrationRule (int order)
-  {
-    IntegrationRule irsegm(ET_SEGM, order);
-    IntegrationRule irhex (ET_HEX, order);    
-
-    Array<Vec<4>> Duffies;
-    Array<double> weights;
-
-    // Sauter-Schwab integration points: [(0,0), (1,0), (1,1)]
-    for (auto ipeta : irhex)
-      for (auto ipxi : irsegm)
-        {
-          double e1 = ipeta(0);
-          double e2 = ipeta(1);
-          double e3 = ipeta(2);
-          double xi = ipxi(0);
-
-          Duffies.Append (xi*Vec<4>(1, e1, e2, e2*e3 ));
-          Duffies.Append (xi*Vec<4>(e2, e2*e3, 1, e1 ));
-          for (int j = 0; j < 2; j++)
-            weights.Append (xi*xi*xi*e2  * ipeta.Weight()*ipxi.Weight());
-        }
-
-    // trafo to [(0,0), (1,0), (0,1)]
-    Array<Vec<2>> ipx, ipy;
-    for (auto ip : Duffies)
-      {
-        ipx += Vec<2>(ip(0)-ip(1), ip(1));
-        ipy += Vec<2>(ip(2)-ip(3), ip(3));
-      }
-
-    return tuple { ipx, ipy, weights };
-  }
-
-
-  // x, y in triangle [(0,0), (1,0), (0,1)]
-  // x in [(0,0),(1,0)] and y in [(0,0),(1,0)] are common edges
-  tuple<Array<Vec<2>>, Array<Vec<2>>, Array<double>> CommonEdgeIntegrationRule (int order)
-  {
-    IntegrationRule irsegm(ET_SEGM, order);
-    IntegrationRule irhex (ET_HEX, order);    
-
-    Array<Vec<4>> Duffies;
-    Array<double> weights;
-
-    // Sauter-Schwab integration points: [(0,0), (1,0), (1,1)]
-    for (auto ipeta : irhex)
-      for (auto ipxi : irsegm)
-        {
-          double e1 = ipeta(0);
-          double e2 = ipeta(1);
-          double e3 = ipeta(2);
-          double xi = ipxi(0);
-          
-          
-          Duffies.Append (xi*Vec<4>(1, e1*e3, 1-e1*e2, e1*(1-e2)));
-          Duffies.Append (xi*Vec<4>(1, e1, 1-e1*e2*e3, e1*e2*(1-e3)));
-          Duffies.Append (xi*Vec<4>(1-e1*e2, e1*(1-e2), 1, e1*e2*e3));
-          Duffies.Append (xi*Vec<4>(1-e1*e2*e3, e1*e2*(1-e3), 1, e1));
-          Duffies.Append (xi*Vec<4>(1-e1*e2*e3, e1*(1-e2*e3), 1, e1*e2));
-          
-          weights.Append (xi*xi*xi*e1*e1    * ipeta.Weight()*ipxi.Weight());
-          for (int j = 0; j < 4; j++)
-            weights.Append (xi*xi*xi*e1*e1*e2 * ipeta.Weight()*ipxi.Weight());          
-        }
-
-    Array<Vec<2>> ipx, ipy;
-    for (auto ip : Duffies)
-      {
-        ipx += Vec<2>(ip(0)-ip(1), ip(1));
-        ipy += Vec<2>(ip(2)-ip(3), ip(3));
-      }
-
-    return tuple { ipx, ipy, weights };
-  }
   
   SingleLayerPotentialOperator :: SingleLayerPotentialOperator(shared_ptr<FESpace> aspace, struct BEMParameters _param)
     : space(aspace), param(_param)
