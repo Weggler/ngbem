13a48f83ada3b5b8bf715871ae7c62ef11493a10
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2024-01-14 18:27:06 +0100

    potential eval - intrule,simd

--- src/ngbem.cpp
+++ src/ngbem.cpp
@@ -1149,22 +1149,22 @@ namespace ngbem
 
           IntegrationRule ir(fel.ElementType(), param.intorder);
           SIMD_IntegrationRule simd_ir(ir);
-          SIMD_MappedIntegrationRule<2,3> mir(simd_ir, trafo, lh);
-          FlatMatrix<SIMD<T>> vals(evaluator->Dim(), mir.Size(), lh);
+          SIMD_MappedIntegrationRule<2,3> miry(simd_ir, trafo, lh);
+          FlatMatrix<SIMD<T>> vals(evaluator->Dim(), miry.Size(), lh);
           
-          evaluator->Apply (fel, mir, elvec, vals);
-          for (int j = 0; j < mir.Size(); j++)
+          evaluator->Apply (fel, miry, elvec, vals);
+          for (int iy = 0; iy < miry.Size(); iy++)
             {
-              Vec<3,SIMD<double>> x = mir[j].GetPoint();
-              Vec<3,SIMD<double>> y = mip23.GetPoint();
-              
-              Vec<3,SIMD<double>> nx = mir[j].GetNV();
-              Vec<3,SIMD<double>> ny = mip23.GetNV();
+              Vec<3,SIMD<double>> x = mip23.GetPoint();
+              Vec<3,SIMD<double>> nx = mip23.GetNV();
+
+              Vec<3,SIMD<double>> y = miry[iy].GetPoint();
+              Vec<3,SIMD<double>> ny = miry[iy].GetNV();
               
               for (auto term : kernel.terms)
                 {
                   auto kernel_ = kernel.Evaluate(x, y, nx, ny)(term.kernel_comp);
-                  simd_result(term.test_comp) += mir[j].GetWeight()*kernel_ * vals(term.trial_comp,j);
+                  simd_result(term.test_comp) += miry[iy].GetWeight()*kernel_ * vals(term.trial_comp,iy);
                 }
             }
         }
@@ -1174,56 +1174,64 @@ namespace ngbem
 
 
   template <typename KERNEL> template <typename T>
-  void PotentialCF<KERNEL> :: T_Evaluate(const BaseMappedIntegrationRule & mir,
+  void PotentialCF<KERNEL> :: T_Evaluate(const BaseMappedIntegrationRule & bmir,
                                          BareSliceMatrix<T> result) const
   {
-    static Timer t("ngbem evaluate potential (ip)"); RegionTimer reg(t);
-    LocalHeapMem<100000> lh("Potential::Eval");
-    auto space = this->gf->GetFESpace();
-    auto mesh = space->GetMeshAccess();
+    try
+      {
+        static Timer t("ngbem evaluate potential (ip)"); RegionTimer reg(t);
+        LocalHeapMem<100000> lh("Potential::Eval");
+        auto space = this->gf->GetFESpace();
+        auto mesh = space->GetMeshAccess();
 
-    Matrix<SIMD<T>> simd_result(Dimension(), mir.Size());
-    simd_result = SIMD<T>(0.0);
-    auto & miry = dynamic_cast<const MappedIntegrationRule<2,3>&>(mir);
-    if constexpr (std::is_same<typename KERNEL::value_type,T>())
-      for (size_t i = 0; i < mesh->GetNSE(); i++)
-        {
-          HeapReset hr(lh);
-          ElementId ei(BND, i);
-          
-          const FiniteElement &fel = space->GetFE(ei, lh);
-          const ElementTransformation &trafo = mesh->GetTrafo(ei, lh);
-          
-          Array<DofId> dnums(fel.GetNDof(), lh);
-          space->GetDofNrs(ei, dnums);
-          FlatVector<T> elvec(fel.GetNDof(), lh);
-          gf->GetElementVector(dnums, elvec);
-
-          IntegrationRule ir(fel.ElementType(), param.intorder);
-          SIMD_IntegrationRule simd_ir(ir);
-          SIMD_MappedIntegrationRule<2,3> mir(simd_ir, trafo, lh);
-          FlatMatrix<SIMD<T>> vals(evaluator->Dim(), mir.Size(), lh);
-          
-          evaluator->Apply (fel, mir, elvec, vals);
-          for (int iy = 0; iy < miry.Size(); iy++)
-            for (int j = 0; j < mir.Size(); j++)
-              {
-                Vec<3,SIMD<double>> x = mir[j].GetPoint();
-                Vec<3,SIMD<double>> y = miry[iy].GetPoint();
-                
-                Vec<3,SIMD<double>> nx = mir[j].GetNV();
-                Vec<3,SIMD<double>> ny = miry[iy].GetNV();
-                
-                for (auto term : kernel.terms)
+        auto & mirx = dynamic_cast<const MappedIntegrationRule<2,3>&>(bmir);
+        
+        Matrix<SIMD<T>> simd_result(Dimension(), mirx.Size());
+        simd_result = SIMD<T>(0.0);
+        if constexpr (std::is_same<typename KERNEL::value_type,T>())
+          for (size_t i = 0; i < mesh->GetNSE(); i++)
+            {
+              HeapReset hr(lh);
+              ElementId ei(BND, i);
+              
+              const FiniteElement &fel = space->GetFE(ei, lh);
+              const ElementTransformation &trafo = mesh->GetTrafo(ei, lh);
+              
+              Array<DofId> dnums(fel.GetNDof(), lh);
+              space->GetDofNrs(ei, dnums);
+              FlatVector<T> elvec(fel.GetNDof(), lh);
+              gf->GetElementVector(dnums, elvec);
+              
+              IntegrationRule ir(fel.ElementType(), param.intorder);
+              SIMD_IntegrationRule simd_ir(ir);
+              SIMD_MappedIntegrationRule<2,3> miry(simd_ir, trafo, lh);
+              FlatMatrix<SIMD<T>> vals(evaluator->Dim(), miry.Size(), lh);
+              
+              evaluator->Apply (fel, miry, elvec, vals);
+              for (int ix = 0; ix < mirx.Size(); ix++)
+                for (int iy = 0; iy < miry.Size(); iy++)
                   {
-                    auto kernel_ = kernel.Evaluate(x, y, nx, ny)(term.kernel_comp);
-                    simd_result(term.test_comp, iy) += mir[j].GetWeight()*kernel_ * vals(term.trial_comp,j);
+                    Vec<3,SIMD<double>> x = mirx[ix].GetPoint();
+                    Vec<3,SIMD<double>> y = miry[iy].GetPoint();
+                    
+                    Vec<3,SIMD<double>> nx = mirx[ix].GetNV();
+                    Vec<3,SIMD<double>> ny = miry[iy].GetNV();
+                    
+                    for (auto term : kernel.terms)
+                      {
+                        auto kernel_ = kernel.Evaluate(x, y, nx, ny)(term.kernel_comp);
+                        simd_result(term.test_comp, ix) += miry[iy].GetWeight()*kernel_ * vals(term.trial_comp,iy);
+                      }
                   }
-              }
-        }
-    for (int i = 0; i < Dimension(); i++)
-      for (int j = 0; j < miry.Size(); j++)
-        result(j, i) = HSum(simd_result(i,j));
+            }
+        for (int i = 0; i < Dimension(); i++)
+          for (int j = 0; j < mirx.Size(); j++)
+            result(j, i) = HSum(simd_result(i,j));
+      }
+    catch (ExceptionNOSIMD & e)
+      {
+        *testout << "exception: " << e.What() << endl;
+      }
   }
 
   
