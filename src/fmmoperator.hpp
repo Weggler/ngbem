03c0b7f6c7bb0bdc9b12abfe10d6a863a63f4d28
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2024-07-26 23:20:07 +0200

    restructure FMM_Operator

--- src/fmmoperator.hpp
+++ src/fmmoperator.hpp
@@ -2,9 +2,9 @@
 #define FILE_FMMOPERATOR
 
 #ifdef USE_KiFMM
-namespace kifmm {
+// namespace kifmm {
   #include <kifmm_rs.h>
-}
+// }
 #endif // USE_KiFMM
 
 #ifdef USE_FMM3D
@@ -33,23 +33,44 @@ namespace ngbem
     return 1.0 / (4*M_PI) / L2Norm(px-py);
   }
 
+
+  template <typename TSCAL>
+  class Base_FMM_Operator : public BaseMatrix
+  {
+  protected:
+    Array<Vec<3>> xpts, ypts, xnv, ynv;
+  public:
+    Base_FMM_Operator(Array<Vec<3>> _xpts, Array<Vec<3>> _ypts,
+                   Array<Vec<3>> _xnv, Array<Vec<3>> _ynv)
+      : xpts(std::move(_xpts)), ypts(std::move(_ypts)),
+        xnv(std::move(_xnv)), ynv(std::move(_ynv))
+    { }
+    
+    AutoVector CreateRowVector () const override
+    {
+      return make_unique<VVector<TSCAL>>(xpts.Size());
+    }
+    AutoVector CreateColVector () const override
+    {
+      return make_unique<VVector<TSCAL>>(ypts.Size());
+    }
+  };
+
+
+  
   template <typename KERNEL>
-  class FMM_Operator : public BaseMatrix
+  class FMM_Operator : public Base_FMM_Operator<typename KERNEL::value_type>
   {
     KERNEL kernel;
-    Array<Vec<3>> xpts, ypts, xnv, ynv;
-#ifdef USE_KiFMM
-    kifmm::LaplaceFft64* fmm;
-    Array<size_t> expansion_order;
-#endif // USE_KiFMM
+    typedef Base_FMM_Operator<typename KERNEL::value_type> BASE;
+    using BASE::xpts, BASE::ypts, BASE::xnv, BASE::ynv;
 
   public:
     FMM_Operator(KERNEL _kernel, Array<Vec<3>> _xpts, Array<Vec<3>> _ypts,
                  Array<Vec<3>> _xnv, Array<Vec<3>> _ynv)
-      : kernel(_kernel), xpts(std::move(_xpts)), ypts(std::move(_ypts)),
-        xnv(std::move(_xnv)), ynv(std::move(_ynv))
-    {
-    }
+      : BASE(std::move(_xpts), std::move( _ypts), std::move(_xnv), std::move(_ynv)),
+      kernel(_kernel)
+    { }
 
     void Mult(const BaseVector & x, BaseVector & y) const override
     {
@@ -61,61 +82,12 @@ namespace ngbem
       fy = 0;
       if constexpr (std::is_same<KERNEL, class LaplaceSLKernel<3>>())
         {
-#ifdef USE_KiFMM
-          static Timer t("KiFMM Apply");
-          RegionTimer r(t);
-          Array<size_t> expansion_order =
-            { 5 };
-          auto fmm = kifmm::laplace_blas_svd_f64_alloc
-            (expansion_order.Data(), expansion_order.Size(),
-             xpts[0].Data(), xpts.Size() * 3,
-             ypts[0].Data(), ypts.Size() * 3,
-             fx.Data(), fx.Size(), // charges
-             true,                       // prune empty
-             150,                        // n_crit
-             0,                          // depth
-             1e-8);
-          kifmm::evaluate(fmm, false);
-          auto potentials = kifmm::potentials(fmm);
-          fy = FlatVector<double>(potentials.len, (double*) potentials.data);
-#elif USE_FMM3D
-          static Timer t("nbem - FMM3D Apply Laplace");
-          RegionTimer r(t);
-          double eps = 1e-8;
-          int ier;
-          int size_x = xpts.Size();
-          int size_y = ypts.Size();
-          lfmm3d_t_c_p_(&eps, &size_x, xpts[0].Data(),
-                       fx.Data(), &size_y, ypts[0].Data(),
-                       fy.Data(), &ier);
-          if (ier != 0)
-            throw Exception("FMM3D failed with err code " + std::to_string(ier));
-          y *= 1.0 / (4*M_PI);
-#else // USE_FMM3D (also no KiFMM)
-        for (size_t ix = 0; ix < xpts.Size(); ix++)
-          for (size_t iy = 0; iy < ypts.Size(); iy++)
-            fy(iy) += __Kernel(xpts[ix], ypts[iy]) * fx(ix);
-#endif // USE_KiFMM
-
+          for (size_t ix = 0; ix < xpts.Size(); ix++)
+            for (size_t iy = 0; iy < ypts.Size(); iy++)
+              fy(iy) += __Kernel(xpts[ix], ypts[iy]) * fx(ix);
         }
       else if constexpr (std::is_same<KERNEL, class HelmholtzSLKernel<3>>())
         {
-#ifdef USE_KiFMM
-#elif USE_FMM3D
-          static Timer t("nbem - FMM3D Apply Helmholtz");
-          RegionTimer r(t);
-          double eps = 1e-8;
-          int ier;
-          std::complex<double> zk = kernel.GetKappa();
-          int size_x = xpts.Size();
-          int size_y = ypts.Size();
-          hfmm3d_t_c_p_(&eps, &zk, &size_x, xpts[0].Data(),
-                        fx.Data(), &size_y, ypts[0].Data(),
-                        fy.Data(), &ier);
-          if (ier != 0)
-            throw Exception("FMM3D failed with err code " + std::to_string(ier));
-          y *= 1.0 / (4*M_PI);
-#else // USE_FMM3D
           for (size_t ix = 0; ix < xpts.Size(); ix++)
             for (size_t iy = 0; iy < ypts.Size(); iy++)
               {
@@ -126,37 +98,9 @@ namespace ngbem
                     fy(iy) += kern * fx(ix);
                   }
               }
-#endif // USE_FMM3D
         }
-
-
       else if constexpr (std::is_same<KERNEL, class CombinedFieldKernel<3>>())
         {
-#ifdef USE_KiFMM
-          // todo
-#elif USE_FMM3D
-          static Timer t("nbem - FMM3D Apply Helmholtz");
-          RegionTimer r(t);
-          double eps = 1e-8;
-          int ier;
-          std::complex<double> zk = kernel.GetKappa();
-          Vector<std::complex<double>> c(xpts.Size());
-          for(auto i : Range(c))
-            c[i] = -Complex(0, kernel.GetKappa()) * fx[i];
-          Vector<Vec<3, std::complex<double>>> v(xpts.Size());
-          for(auto i : Range(xnv))
-            v[i] = fx[i] * xnv[i];
-          int size_x = xpts.Size();
-          int size_y = ypts.Size();
-          hfmm3d_t_cd_p_(&eps, &zk, &size_x, xpts[0].Data(),
-                         c.Data(), v[0].Data(),
-                         &size_y, ypts[0].Data(),
-                         fy.Data(), &ier);
-          if (ier != 0)
-            throw Exception("FMM3D failed with err code " + std::to_string(ier));
-          for(auto i : Range(fy))
-            fy[i] *= 1 / (4*M_PI);
-#else // USE_FMM3D
           /*
             T norm = L2Norm(x-y);
             T nxy = InnerProduct(ny, (x-y));
@@ -180,23 +124,231 @@ namespace ngbem
                     fy(iy) += kern * fx(ix);                    
                   }
               }
-#endif // USE_FMM3D || USE_KiFMM
         }
       else
         throw Exception("fmm not available");
     }
 
+  };
 
-    AutoVector CreateRowVector () const override
+  
+
+  
+#ifdef USE_FMM3D
+  
+  // we can specialze the complete class ... 
+  template <> 
+  class FMM_Operator<LaplaceSLKernel<3>> : public Base_FMM_Operator<double>
+  {
+    typedef LaplaceSLKernel<3> KERNEL;
+    typedef Base_FMM_Operator<double> BASE;
+    
+    KERNEL kernel;
+    using BASE::xpts, BASE::ypts, BASE::xnv, BASE::ynv;    
+
+  public:
+    FMM_Operator(KERNEL _kernel, Array<Vec<3>> _xpts, Array<Vec<3>> _ypts,
+                 Array<Vec<3>> _xnv, Array<Vec<3>> _ynv)
+      : BASE(std::move(_xpts), std::move( _ypts), std::move(_xnv), std::move(_ynv)),
+        kernel(_kernel)
+    { }
+
+    void Mult(const BaseVector & x, BaseVector & y) const override
     {
-      return make_unique<VVector<typename KERNEL::value_type>>(xpts.Size());
-    }
-    AutoVector CreateColVector () const override
-    {
-      return make_unique<VVector<typename KERNEL::value_type>>(ypts.Size());
+      static Timer tall("ngbem fmm apply LaplaceSL (FMM3D)"); RegionTimer reg(tall);
+      auto fx = x.FV<double>();
+      auto fy = y.FV<double>();
+      
+      fy = 0;
+
+      double eps = 1e-8;
+      int ier;
+      int size_x = xpts.Size();
+      int size_y = ypts.Size();
+      lfmm3d_t_c_p_(&eps, &size_x, xpts[0].Data(),
+                    fx.Data(), &size_y, ypts[0].Data(),
+                    fy.Data(), &ier);
+      if (ier != 0)
+        throw Exception("FMM3D failed with err code " + std::to_string(ier));
+      y *= 1.0 / (4*M_PI);
     }
   };
+
+  
+  // or just specialze the apply method:
+
+  template <>
+  void FMM_Operator<HelmholtzSLKernel<3>> :: Mult(const BaseVector & x, BaseVector & y) const 
+  {
+    static Timer tall("ngbem fmm apply HelmholtzSL (FMM3D)"); RegionTimer reg(tall);
+    auto fx = x.FV<Complex>();
+    auto fy = y.FV<Complex>();
+
+    fy = 0;
+
+    double eps = 1e-8;
+    int ier;
+    std::complex<double> zk = kernel.GetKappa();
+    int size_x = xpts.Size();
+    int size_y = ypts.Size();
+    hfmm3d_t_c_p_(&eps, &zk, &size_x, xpts[0].Data(),
+                  fx.Data(), &size_y, ypts[0].Data(),
+                  fy.Data(), &ier);
+    if (ier != 0)
+      throw Exception("FMM3D failed with err code " + std::to_string(ier));
+    y *= 1.0 / (4*M_PI);
+  }
+
+  
+  template <>
+  void FMM_Operator<CombinedFieldKernel<3>> :: Mult(const BaseVector & x, BaseVector & y) const 
+  {
+    static Timer tall("ngbem fmm apply CombinedField (FMM3D)"); RegionTimer reg(tall);
+    auto fx = x.FV<Complex>();
+    auto fy = y.FV<Complex>();
+    
+    fy = 0;
+    
+    double eps = 1e-8;
+    int ier;
+    Complex zk = kernel.GetKappa();
+    Vector<Complex> c(xpts.Size());
+    for(auto i : Range(c))
+      c[i] = -Complex(0, kernel.GetKappa()) * fx[i];
+    Vector<Vec<3, Complex>> v(xpts.Size());
+    for(auto i : Range(xnv))
+      v[i] = fx[i] * xnv[i];
+    int size_x = xpts.Size();
+    int size_y = ypts.Size();
+    hfmm3d_t_cd_p_(&eps, &zk, &size_x, xpts[0].Data(),
+                   c.Data(), v[0].Data(),
+                   &size_y, ypts[0].Data(),
+                   fy.Data(), &ier);
+    if (ier != 0)
+      throw Exception("FMM3D failed with err code " + std::to_string(ier));
+    for(auto i : Range(fy))
+      fy[i] *= 1 / (4*M_PI);
+  }
+
+#endif // USE_FMM3D
+
+
+#ifdef USE_KiFMM
+
+  // we can specialze the complete class ... 
+  template <> 
+  class FMM_Operator<LaplaceSLKernel<3>> : public Base_FMM_Operator<double>
+  {
+    typedef LaplaceSLKernel<3> KERNEL;
+    typedef Base_FMM_Operator<double> BASE;
+    
+    KERNEL kernel;
+    using BASE::xpts, BASE::ypts, BASE::xnv, BASE::ynv;
+
+    // LaplaceFft64* fmm;
+    // Array<size_t> expansion_order;
+
+  public:
+    FMM_Operator(KERNEL _kernel, Array<Vec<3>> _xpts, Array<Vec<3>> _ypts,
+                 Array<Vec<3>> _xnv, Array<Vec<3>> _ynv)
+      : BASE(std::move(_xpts), std::move( _ypts), std::move(_xnv), std::move(_ynv)),
+        kernel(_kernel)
+    {
+      // do some initialization ? 
+    }
+
+    void Mult(const BaseVector & x, BaseVector & y) const override
+    {
+      static Timer tall("ngbem fmm apply LaplaceSL (FMM3D)"); RegionTimer reg(tall);
+      auto fx = x.FV<double>();
+      auto fy = y.FV<double>();
+
+      // following: https://github.com/bempp/kifmm/blob/enh/c-abi/kifmm/c/main.c
+      bool prune_empty = true;
+      uint64_t n_crit = 150;
+      uint64_t depth = 0;
+      double singular_value_threshold = 0.001;
+      
+      uintptr_t expansion_order[] = {6};
+      uintptr_t nexpansion_order = 1;
+      uintptr_t block_size = 32;
+
+      // Instantiate a Laplace evaluator
+      struct FmmEvaluator *evaluator =
+        laplace_fft_f64_alloc(
+                              expansion_order, nexpansion_order,
+                              xpts[0].Data(), 3*xpts.Size(), 
+                              ypts[0].Data(), 3*ypts.Size(), 
+                              fx.Data(), fx.Size(), 
+                              prune_empty, n_crit, depth, block_size
+                              );
+      
+      bool timed = true;
+      evaluate(evaluator, timed);
+
+      MortonKeys *leaves = leaves_target_tree(evaluator);
+      
+      // printf("Number of leaf keys (n): %zu\n", leaves->len);
+      // for (uintptr_t i = 0; i < 5; ++i) {
+      // printf("Element %zu: %llu\n", i, leaves->data[i]);
+      // }
+
+      Potentials *potentials = leaf_potentials(evaluator, leaves->data[123]);
+
+      /*
+      Coordinates *coordinates =
+        coordinates_source_tree(evaluator, leaves->data[123]);
+      printf("Number of coordinates: %zu\n", coordinates->len);
+
+      const double *coords = (const double *)coordinates->data;
+      for (uintptr_t i = 0; i < 5; ++i) {
+        printf("Element %zu: [%f, %f, %f]\n", i, coords[i * 3], coords[i * 3 + 1],
+               coords[i * 3 + 2]);
+      }
+      */
+      
+      // printf("Number of potentials: %zu\n", potentials->n);
+      Potential *pot = &potentials->data[0];
+      const double *data = (const double *)pot->data;
+      for (uintptr_t i = 0; i < 5; ++i) {
+        // printf("Element %zu: %f\n", i, data[i]);
+        fy(i) = data[i];
+      }
+
+      // Cleanup
+      free_fmm_evaluator(evaluator);
+      free_morton_keys(leaves);
+      free(potentials);
+
+
+      
+      /*
+      fy = 0;
+
+      Array<size_t> expansion_order =
+        { 5 };
+      auto fmm = laplace_blas_svd_f64_alloc
+        (expansion_order.Data(), expansion_order.Size(),
+         xpts[0].Data(), xpts.Size() * 3,
+         ypts[0].Data(), ypts.Size() * 3,
+         fx.Data(), fx.Size(), // charges
+         true,                       // prune empty
+         150,                        // n_crit
+         0,                          // depth
+         1e-8);
+      evaluate(fmm, false);
+      auto potentials = potentials(fmm);
+      fy = FlatVector<double>(potentials.len, (double*) potentials.data);
+      */
+      
+    }
+  };
+
+#endif // KiFMM
+  
+  
 }
+
 
 #endif
 
