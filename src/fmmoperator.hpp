424bb440f9c375bfa434d5387a879fd307a3611a
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2024-08-23 08:53:34 +0200

    ngfmm

--- src/fmmoperator.hpp
+++ src/fmmoperator.hpp
@@ -1,5 +1,6 @@
 #ifndef FILE_FMMOPERATOR
 #define FILE_FMMOPERATOR
+#include <mptools.hpp>
 
 #ifdef USE_KiFMM
 // namespace kifmm {
@@ -139,8 +140,163 @@ namespace ngbem
 
   };
 
-  
+  // #define USE_NGFMM
+#ifdef USE_NGFMM
 
+  template <>
+  void FMM_Operator<HelmholtzSLKernel<3>> :: Mult(const BaseVector & x, BaseVector & y) const 
+  {
+    static Timer tall("ngbem fmm apply CombinedField (ngfmm)"); RegionTimer reg(tall);
+    auto fx = x.FV<Complex>();
+    auto fy = y.FV<Complex>();
+
+    fy = 0;
+    
+    double kappa = kernel.GetKappa();
+
+    
+    Vec<3> xmax(-1e99, -1e99, -1e99);
+    Vec<3> xmin(1e99, 1e99, 1e99);
+    for (auto xi : xpts)
+      {
+        for (int j = 0; j < 3; j++)
+          {
+            xmin(j) = min(xmin(j), xi(j));
+            xmax(j) = max(xmax(j), xi(j));
+          }
+      }
+
+    Vec<3> cx = 0.5*(xmin+xmax);
+    double rx = 0;
+    for (int j = 0; j < 3; j++)
+      rx = max(rx, xmax(j)-xmin(j));
+
+    Vec<3> ymax(-1e99, -1e99, -1e99);
+    Vec<3> ymin(1e99, 1e99, 1e99);
+    for (auto yi : ypts)
+      {
+        for (int j = 0; j < 3; j++)
+          {
+            ymin(j) = min(ymin(j), yi(j));
+            ymax(j) = max(ymax(j), yi(j));
+          }
+      }
+
+    Vec<3> cy = 0.5*(ymin+ymax);
+    double ry = 0;
+    for (int j = 0; j < 3; j++)
+      ry = max(ry, ymax(j)-ymin(j));
+
+
+    
+    auto singmp = make_shared<SingularMLMultiPole>(cx, rx, int(3*kappa*rx), kappa);
+
+    for (int i = 0; i < xpts.Size(); i++)
+      singmp->AddCharge(xpts[i], fx(i));
+
+    singmp->CalcMP();
+
+    /*
+    RegularMLMultiPole regmp (singmp, cy, ry, int(3*kappa*ry));
+    */
+    RegularMLMultiPole regmp (cy, ry, int(3*kappa*ry), kappa);
+    for (int i = 0; i < ypts.Size(); i++)
+      regmp.AddTarget(ypts[i]);
+    regmp.CalcMP(singmp);
+
+    
+    // for (int i = 0; i < ypts.Size(); i++)
+    ParallelFor (ypts.Size(), [&](int i) {
+      fy(i) = regmp.Evaluate(ypts[i]);
+    });
+  }
+
+
+
+  
+  template <>
+  void FMM_Operator<CombinedFieldKernel<3>> :: Mult(const BaseVector & x, BaseVector & y) const 
+  {
+    static Timer tall("ngbem fmm apply CombinedField (ngfmm)"); RegionTimer reg(tall);
+    static Timer t1("ngbem fmm apply CombinedField (ngfmm) find center/rad");
+    static Timer t2("ngbem fmm apply CombinedField (ngfmm) sing");
+    static Timer t3("ngbem fmm apply CombinedField (ngfmm) reg");
+    static Timer t4("ngbem fmm apply CombinedField (ngfmm) eval");        
+    auto fx = x.FV<Complex>();
+    auto fy = y.FV<Complex>();
+
+    fy = 0;
+    if (L2Norm(x) == 0) return;
+    
+    t1.Start();
+    
+    Vec<3> xmax(-1e99, -1e99, -1e99);
+    Vec<3> xmin(1e99, 1e99, 1e99);
+    for (auto xi : xpts)
+      {
+        for (int j = 0; j < 3; j++)
+          {
+            xmin(j) = min(xmin(j), xi(j));
+            xmax(j) = max(xmax(j), xi(j));
+          }
+      }
+
+    double kappa = kernel.GetKappa();
+    Vec<3> cx = 0.5*(xmin+xmax);
+    double rx = 0;
+    for (int j = 0; j < 3; j++)
+      rx = max(rx, xmax(j)-xmin(j));
+
+
+    Vec<3> ymax(-1e99, -1e99, -1e99);
+    Vec<3> ymin(1e99, 1e99, 1e99);
+    for (auto yi : ypts)
+      {
+        for (int j = 0; j < 3; j++)
+          {
+            ymin(j) = min(ymin(j), yi(j));
+            ymax(j) = max(ymax(j), yi(j));
+          }
+      }
+
+    Vec<3> cy = 0.5*(ymin+ymax);
+    double ry = 0;
+    for (int j = 0; j < 3; j++)
+      ry = max(ry, ymax(j)-ymin(j));
+
+    t1.Stop();
+    t2.Start();
+    
+    auto singmp = make_shared<SingularMLMultiPole>(cx, rx, int(2*kappa*rx), kappa);
+
+    for (int i = 0; i < xpts.Size(); i++)
+      {
+        singmp->AddCharge(xpts[i], Complex(0,-kappa)*fx(i));
+        singmp->AddDipole(xpts[i], -xnv[i], fx(i));        
+      }
+    singmp->CalcMP();
+    cout << "sing norm = " << singmp->Norm() << endl;
+    t2.Stop();
+    t3.Start();
+    // RegularMLMultiPole regmp (singmp, cy, ry, int(2*kappa*ry));
+
+    RegularMLMultiPole regmp (cy, ry, int(3*kappa*ry), kappa);
+    for (int i = 0; i < ypts.Size(); i++)
+      regmp.AddTarget(ypts[i]);
+
+    regmp.CalcMP(singmp);
+    cout << "reg norm = " << regmp.Norm() << endl;
+    t3.Stop();
+    t4.Start();
+    // for (int i = 0; i < ypts.Size(); i++)
+    // fy(i) = regmp.Evaluate(ypts[i]);
+
+    ParallelFor (ypts.Size(), [&](int i) {
+      fy(i) = regmp.Evaluate(ypts[i]);
+    });
+    t4.Stop();
+  }
+#endif 
   
 #ifdef USE_FMM3D
   
